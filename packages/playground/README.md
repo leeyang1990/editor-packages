38135d94f42782d4f41ba7ea95de1cc884d88e8c
liyang <leeyang1990@gmail.com>
2021-01-24 11:42:55 +0800

    update info

--- packages/playground/README.md
+++ packages/playground/README.md
@@ -1,1 +1,10 @@
 # playground-electron
+
+### electron 编辑器壳程序
+#### 框架设计
+-- layout管理
+
+-- 独立渲染模块
+
+-- ipc通信模块
+
--- packages/playground/src/playground/index.ts
+++ packages/playground/src/playground/index.ts
@@ -1,5 +1,7 @@
+import GoldenLayout = require('golden-layout');
 import { LitElement, html, css, customElement, property } from 'lit-element';
-
+import "golden-layout/src/css/goldenlayout-base.css";
+import "golden-layout/src/css/goldenlayout-dark-theme.css";
 @customElement('e-playground')
 class Index extends LitElement {
 
@@ -14,9 +16,46 @@ class Index extends LitElement {
 				justify-content: center;
 				display: flex;
 			}
+			:host{
+				width: 100%;
+				height: 100%;
+			}
 		`;
 	}
-
+	firstUpdated(){
+		var config = {
+			content: [{
+			  type: 'column',
+			  content: [
+				  {
+				  type:'component',
+				  componentName: 'example',
+				  componentState: { text: 'Component 1' }
+				  },
+				{
+				  type:'component',
+				  componentName: 'example',
+				  componentState: { text: 'Component 2' }
+				  },
+				{
+				  type:'component',
+				  componentName: 'example',
+				  componentState: { text: 'Component 3' }
+				  }
+			  ]
+			}]
+		  };
+		  console.log(this.shadowRoot!.querySelector('.content') )
+		  let root = document.getElementById('root');
+		//   var myLayout = new GoldenLayout( config,this.shadowRoot!.querySelector('.content') );
+		  var myLayout = new GoldenLayout( config,root );
+		  
+		  myLayout.registerComponent( 'example', function( container:any, state:any ){
+			// container.getElement().html( '<h2>' + state.text + '</h2>');
+		  });
+		  
+		  myLayout.init();
+	}
     // Implement `render` to define a template for your element.
     render() {
         return html`<div class="content">playground</div>`
